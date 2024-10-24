abf6c49d62414d1c1d12dcd50134c793a71c6bbd
Hyun <jjjtoy@naver.com>
2024-10-24 17:49:05 +0900

    feat: 비밀 정보 보호 처리

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://jjjangtoy7:gus125@boilerplate.62nhn.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("mongoDB connected!!!!"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  //회원 가입 시 필요한 정보를 클라이언트에서
+  //가져오면 db에 넣기
+
+  const user = new User(req.body);
+  await user
+    .save()
+    .then(() => {
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({ success: false, err: err });
+    });
 });
 
 app.listen(port, () => {
